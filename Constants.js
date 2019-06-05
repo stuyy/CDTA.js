3a55da233ec0bd085d13859e32a7326e2366348d
ansonfoong <afoong@albany.edu>
2019-06-05 11:29:17 -0400

    Updated Constants. Added a ROUTES constant which is an object that holds key-value pairs of each route

--- Constants.js
+++ Constants.js
@@ -1,3 +1,14 @@
 const API = "http://api.cdta.org/api/v1/?request=";
-
+const ROUTES = {
+    TIME: 'time',
+    ROUTES: 'routes',
+    DIRECTIONS: 'directions',
+    SCHEDULES: 'schedules',
+    STOPS: 'stops',
+    NEAR_STOPS: 'nearstops',
+    SEARCH_STOPS: 'searchstops',
+    SEARCH: 'search',
+    ARRIVALS: 'arrivals',
+    ALERTS: 'alerts'
+}
 module.exports = { API };
