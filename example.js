130a8234787902b76ac81c21615de24a0f8bb874
ansonfoong <afoong@albany.edu>
2019-06-06 10:08:31 -0400

    Removed the entire switch case in cdta.js and moved it into the utility function validate.

--- example.js
+++ example.js
@@ -5,30 +5,42 @@ app.on('error', err =>{
     console.log(err);
 });
 
-app.get('time')
-.then(res => console.log(res))
-.catch(err => console.log(err));
+app.on('authorized', () => {
+    console.log("Authorized!");
+});
 
-app.get('routes', 712)
-.then(res => console.log(res))
-.catch(err => console.log(err));
+// app.get('time')
+// .then(res => console.log(res))
+// .catch(err => console.log(err));
 
-app.get('directions', 12)
-.then(res => console.log(res))
-.catch(err => console.log(err));
+// app.get('routes', 712)
+// .then(res => console.log(res))
+// .catch(err => console.log(err));
 
-app.get('schedules', 12, 'weekday', 0)
-.then(res => console.log(res))
-.catch(err => console.log(err));
+// app.get('directions', 12)
+// .then(res => console.log(res))
+// .catch(err => console.log(err));
 
-app.get('stops', 12, 0)
-.then(res => console.log(res))
-.catch(err => console.log(err));
+// app.get('schedules', 12, 'weekday', 0)
+// .then(res => console.log(res))
+// .catch(err => console.log(err));
 
-app.get('searchstops', 'SUNY Collins')
-.then(res => console.log(res))
-.catch(err => console.log(err));
+// app.get('stops', 12, 0)
+// .then(res => console.log(res))
+// .catch(err => console.log(err));
 
-app.get('search', 'collins circle')
-.then(response => console.log(response))
-.catch(err => console.log(err));
+// app.get('searchstops', 'SUNY Collins')
+// .then(res => console.log(res))
+// .catch(err => console.log(err));
+
+// app.get('search', 'collins circle')
+// .then(response => console.log(response))
+// .catch(err => console.log(err));
+
+// app.get('nearstops', 74.65, -73.75)
+// .then(res => console.log(res))
+// .catch(err => console.log(err));
+
+// app.get('arrivals', 12)
+// .then(res => console.log(res))
+// .catch(err => console.log(err));
